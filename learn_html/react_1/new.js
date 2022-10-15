1a40342808180dab6f58a6872e63ce30dc7bda91
ericuxnyc <86964628+ericuxnyc@users.noreply.github.com>
2022-10-15 09:12:43 -0400

    General commit messages

---
+++ learn_html/react_1/new.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class myComponentClass extends Component {
+
+    render() {
+       return <h1>React Component</h1>;
+    }
+}
+
+ReactDOM.render(<MyComponentClass />, document.getElementsByClassName('app'));
---
+++ learn_javascript/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+<
+UpperCase.js,a/a/aaad880e8f4573089ee301c4e24a933ff3250b82
