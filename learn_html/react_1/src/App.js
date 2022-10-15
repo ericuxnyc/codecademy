9cf8433206a89538bf794fc00d3a4e0e06d9c3cd
ericuxnyc <86964628+ericuxnyc@users.noreply.github.com>
2022-10-15 09:13:11 -0400

    General commit messages

---
+++ learn_html/react_1/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  class MyComponentClass extends React.Component {
+    render() {
+      return <h1>Hello world</h1>;
+    }
+  }
+
+  ReactDOM.render(
+      <MyComponentClass />,
+      document.getElementById('app')
+  );
+}
+  /* return (
+     <div className="App">
+       <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+         <p>
+           Edit <code>src/App.js</code> and save to reload.
+         </p>
+         <a
+           className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer"
+         >
+           Learn React
+         </a>
+       </header>
+     </div>
+   );
+ }
+
+ */export default App;
+
+
+
+
